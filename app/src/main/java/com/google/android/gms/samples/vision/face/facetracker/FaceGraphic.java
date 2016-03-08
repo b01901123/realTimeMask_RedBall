5101e10f63eee634e96e0fd5c13522992beeecc0
b01901123 <b01901123@ntu.edu.tw>
2016-03-08 13:47:21 -0800

    red ball

---
+++ app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/FaceGraphic.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.gms.samples.vision.face.facetracker;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.opengl.Matrix;
+
+import com.google.android.gms.samples.vision.face.facetracker.ui.camera.GraphicOverlay;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.Landmark;
+
+/**
+ * Graphic instance for rendering face position, orientation, and landmarks within an associated
+ * graphic overlay view.
+ */
+class FaceGraphic extends GraphicOverlay.Graphic {
+    private static final float FACE_POSITION_RADIUS = 10.0f;
+    private static final float ID_TEXT_SIZE = 40.0f;
+    private static final float ID_Y_OFFSET = 50.0f;
+    private static final float ID_X_OFFSET = -50.0f;
+    private static final float BOX_STROKE_WIDTH = 5.0f;
+    private static Bitmap rabbitEar, redNose;
+    private FaceTrackerActivity activity;
+
+    private static final int COLOR_CHOICES[] = {
+        Color.BLUE,
+        Color.CYAN,
+        Color.GREEN,
+        Color.MAGENTA,
+        Color.RED,
+        Color.WHITE,
+        Color.YELLOW
+    };
+    private static int mCurrentColorIndex = 0;
+
+    private Paint mFacePositionPaint;
+    private Paint mIdPaint;
+    private Paint mBoxPaint;
+
+    private volatile Face mFace;
+    private int mFaceId;
+    private float mFaceHappiness;
+
+    FaceGraphic(GraphicOverlay overlay) {
+        super(overlay);
+
+        mCurrentColorIndex = (mCurrentColorIndex + 1) % COLOR_CHOICES.length;
+        final int selectedColor = COLOR_CHOICES[mCurrentColorIndex];
+
+        mFacePositionPaint = new Paint();
+        mFacePositionPaint.setColor(selectedColor);
+
+        mIdPaint = new Paint();
+        mIdPaint.setColor(selectedColor);
+        mIdPaint.setTextSize(ID_TEXT_SIZE);
+
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(selectedColor);
+        mBoxPaint.setStyle(Paint.Style.STROKE);
+        mBoxPaint.setStrokeWidth(BOX_STROKE_WIDTH);
+        //load redNose, etc.
+        this.redNose = overlay.getFaceTrackerActivity().redNose;
+    }
+
+    void setId(int id) {
+        mFaceId = id;
+    }
+
+
+    /**
+     * Updates the face instance from the detection of the most recent frame.  Invalidates the
+     * relevant portions of the overlay to trigger a redraw.
+     */
+    void updateFace(Face face) {
+        mFace = face;
+        postInvalidate();
+    }
+
+    /**
+     * Draws the face annotations for position on the supplied canvas.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        Face face = mFace;
+        if (face == null) {
+            return;
+        }
+
+        // Draws a circle at the position of the detected face, with the face's track id below.
+        float x = translateX(face.getPosition().x + face.getWidth() / 2);
+        float y = translateY(face.getPosition().y + face.getHeight() / 2);
+        canvas.drawCircle(x, y, FACE_POSITION_RADIUS, mFacePositionPaint);
+        //try: draw a mask
+
+        canvas.drawText("id: " + mFaceId, x + ID_X_OFFSET, y + ID_Y_OFFSET, mIdPaint);
+        canvas.drawText("happiness: " + String.format("%.2f", face.getIsSmilingProbability()), x - ID_X_OFFSET, y - ID_Y_OFFSET, mIdPaint);
+        canvas.drawText("right eye: " + String.format("%.2f", face.getIsRightEyeOpenProbability()), x + ID_X_OFFSET * 2, y + ID_Y_OFFSET * 2, mIdPaint);
+        canvas.drawText("left eye: " + String.format("%.2f", face.getIsLeftEyeOpenProbability()), x - ID_X_OFFSET*2, y - ID_Y_OFFSET*2, mIdPaint);
+
+        // Draws a bounding box around the face.
+        float xOffset = scaleX(face.getWidth() / 2.0f);
+        float yOffset = scaleY(face.getHeight() / 2.0f);
+        float left = x - xOffset;
+        float top = y - yOffset;
+        float right = x + xOffset;
+        float bottom = y + yOffset;
+        canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        //canvas.drawBitmap(redNose,left,top,null);
+
+        //scale
+
+
+        //drawNose(canvas, redNose, face,left,top);
+    }
+    void drawNose(Canvas canvas, Bitmap bitmap,Face face,float left,float top){
+        //scale
+       double viewWidth = canvas.getWidth();
+        double viewHeight = canvas.getHeight();
+        double imageWidth = bitmap.getWidth();
+        double imageHeight = bitmap.getHeight();
+        double scale = Math.min(viewWidth / imageWidth, viewHeight / imageHeight);
+
+        //Landmark landmark = face.getLandmarks().get(2);
+        /*int cx = (int) (landmark.getPosition().x * scale);
+        int cy = (int) (landmark.getPosition().y * scale);*/
+        //rotate the bitmap according to the face rotation
+        //face.getEulerY()  face.getEulerZ()
+        //Canvas canvas1 = new Canvas(redNose);
+        //Matrix matrix = new Matrix();
+        //matrix.setRotate(face.getEulerY());
+        //matrix.setTranslate(cx, cy);
+
+
+        //draw the nose ont the face
+        canvas.drawBitmap(redNose,left,top,null);
+        //canvas.drawBitmap(redNose,cx,cy,null);
+        //canvas.drawBitmap(redNose, matrix, null);
+    }
+    void drawEar(Canvas canvas, Bitmap bitmap){
+        canvas.drawBitmap(bitmap, 0, 0, null);
+    }
+}
